8cf3fb7f3b7a0da7e5798a7643618154fa3bb3dc
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-03-21 21:34:25 +0100

    Added d3 Test drawing svg

---
+++ src/toto.ts
@@ -0,0 +1,11 @@
+import * as d3 from 'd3';
+
+d3.select('body')
+    .append('svg')
+    .attr('width', 400)
+    .attr('height', 300)
+    .append('rect')
+    .attr('width', 50)
+    .attr('height', 50)
+    .attr('x', 50)
+    .attr('y', 50)
