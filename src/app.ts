67e35ae62596ee2b7ae27d59afe9c7226986451d
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-03-24 22:31:06 +0100

    Fiddling with styles

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ d3.json(`file://${__dirname}/data/data.json`, (error, data: Array<any>) => {
     if (error) {
         console.error(error)
     } else {
-        d3.select('.main-container')
+        d3.select('#previews')
             .selectAll('.preview')
-            .data(data.sort((a, b) => b.title - a.title).splice(0, 10))
+            .data(data.sort((a, b) => b.rating - a.rating).splice(0, 12))
             .enter()
+            .append('div')
+            .classed('preview', true)
+            .classed('flex-25', true)
             .append('div')
             .classed('image-preview', true)
             .style('background-image', d => `url('data:image/png;base64,${d.serializedImage}')`)
