883dd8153a7f1bc49d76e7c0796f671e44d6c827
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-03-31 21:22:52 +0200

    Path New component

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
-
+import * as d3 from 'd3';
+import * as path from 'path'
+const projectRootPath = //path.resolve(__dirname);
+process.cwd();
 @Component({
   selector: 'preview',
-  templateUrl: './preview.html'
+  template: `<div class="flex-25" style="display:flex">
+    <div class="preview">
+        <div class="preview-image"></div>
+        <span>{{name}}</span>
+    </div>
+</div>`
 })
 export class PreviewComponent {
   name = 'Angular';
+  constructor() {
+    d3.json(`file://${projectRootPath}/src/data/data.json`, (error, data: Array<any>) => {
+      if (error) {
+        console.error(error)
+      } else {
+        this.name = data[0].title;
+        console.log(this.name)
+      }
+    });
+  }
 }
