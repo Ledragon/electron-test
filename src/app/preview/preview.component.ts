856ee734adf6acf3048e05511e244dbe5a0dfc8a
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-04-01 22:33:19 +0200

    Displaying images

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,22 +1,17 @@
-import { Component, Injectable, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit, Sanitizer } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { libraryService } from '../library.service';
 
 @Component({
   selector: 'preview',
   templateUrl: 'app/preview/preview.html',
   providers: [libraryService]
-  //   template: `<div class="flex-25" style="display:flex">
-  //     <div class="preview">
-  //         <div class="preview-image"></div>
-  //         <span>{{name}}</span>
-  //     </div>
-  // </div>`
 })
 
 export class PreviewComponent implements OnInit {
   videos: Array<any> = [];
 
-  constructor(private _libraryService: libraryService) {
+  constructor(private _libraryService: libraryService, private _sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
@@ -27,11 +22,10 @@ export class PreviewComponent implements OnInit {
       .subscribe((data: Array<any>) => {
         this.videos = data
           .map((d: any) => {
-            d.url = `url('data:image/png;base64,${d.serializedImage}')`;
+            d.url = this._sanitizer.bypassSecurityTrustStyle(`url('data:image/png;base64,${d.serializedImage}')`);
             return d;
           })
           .splice(0, 12);
-        console.log(this.videos)
       });
   }
 
